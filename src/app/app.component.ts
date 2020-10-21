9775c5536f147d1fa73034203dd45ab4154100b6
LAPTOP-9DASBMVF\Ajay <saggidubey@outlook.com>
2020-10-21 15:45:26 +0530

    project MAX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+declare var androidCallback: any;
 
 @Component({
   selector: 'app-root',
@@ -16,12 +17,20 @@ export class AppComponent {
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    androidCallback.appInit();
+
+    window.addEventListener(
+      'callbackAppInit',
+      (data: any) => {
+        alert(data);
+      })
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
     });
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public clientName= 'ACME Fast Food - Manchester';
+  public currentBuildingOccupancy=101;
+  public  totalVisitsToday=1200;
+  public  maximumBuildingOccupancy=150;
+  public  buildingOccupancyRatio=this.currentBuildingOccupancy/this.maximumBuildingOccupancy;
+  public  waitTime = 0;
+  //(this.buildingOccupancyRatio>=1)? 2 : 0;
+  private clikedCounter=0;
+  public showSettingsForm:boolean=false;
   constructor() {}
+
+  buttonClikedCounter(){
+    
+   
+    this.clikedCounter=this.clikedCounter+1;
+    if(this.clikedCounter>4)
+    {
+      this.showSettingsForm=true;
+      this.clikedCounter=0;
+    }
+  }
+  hideForm()
+  {
+    this.showSettingsForm=false;
+  }
 
 }
